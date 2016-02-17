21f3596d4eccd94828a68c25abd8c9f873a1a361
dtkang <dtkang@gmail.com>
2016-02-17 03:21:55 +0000

    Add sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.default_url_options = { host: 'shielded-peak-57897.herokuapp.com' }
+  config.assets.initialize_on_precompile = false
 end
